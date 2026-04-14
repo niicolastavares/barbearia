0233f57181e0168ed5d8f52b59cb97e1704608bc
Nicolas Tavares <nicolastavares2805@gmail.com>
2026-04-14 11:07:02 -0300

    Subindo o Projeto Barbearia~

---
+++ src/resources/views/partials/head.blade.php
@@ -0,0 +1,12 @@
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Home | BARBEARIA LISBOA</title>
+
+        <link rel="stylesheet preload" as="style" href="{{ asset('barbearia/css/preload.min.css') }}" />
+        <link rel="stylesheet preload" as="style" href="{{ asset('barbearia/css/icomoon.css') }}" />
+        <link rel="stylesheet preload" as="style" href="{{ asset('barbearia/css/libs.min.css') }}" />
+
+        <link rel="stylesheet" href="{{ asset('barbearia/css/index.min.css') }}" />
+    </head>
---
+++ src/resources/views/partials/script.blade.php
@@ -0,0 +1,2 @@
+<script src="{{ asset('barberia/js/common.min.js') }}"></script>
+<script src="{{ asset('barbearia/js/index.min.js') }}"></script>
